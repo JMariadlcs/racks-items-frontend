19cf2eefa1d038db559087f82912a6e3552198bc
MrWojack <culokarratu@protonmail.com>
2022-05-14 14:22:59 +0200

    frontend files

---
+++ pages/components/caseopener.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import Image from 'next/image'
+
+export default function opencase() {
+  return (
+    <div className='flex'>
+¡
+        <div className='flex absolute  justify-center marco items-center'>
+          <div className='item1'>
+
+          </div>
+          <div className='item2'></div>
+
+           </div>
+        <div className='flex justify-center'>
+        <section className="slideshow bg-main">
+            <div className='entire-content'>
+                <div className="content-carrousel ">
+                     <figure className='flex justify-center items-center shadow bg-secondary ticker-red'><Image width="100" height="100" src="/reloj.png"/></figure>
+                     <figure className='flex justify-center items-center shadow bg-secondary ticker-pink'><Image width="100" height="100" src="/napa.png"/></figure>
+                     <figure className='flex justify-center items-center shadow bg-secondary ticker-blue'><Image width="100" height="100" src="/guante.png"/></figure>
+                     <figure className='flex justify-center items-center shadow bg-secondary ticker-red'><Image width="100" height="100" src="/reloj.png"/></figure>
+                     <figure className='flex justify-center items-center shadow bg-secondary ticker-pink'><Image width="100" height="100" src="/napa.png"/></figure>
+                     <figure className='flex justify-center items-center shadow bg-secondary ticker-blue'><Image width="100" height="100" src="/guante.png"/></figure>
+                        <figure className='flex justify-center items-center shadow bg-secondary ticker-red'><Image width="100" height="100" src="/reloj.png"/></figure>
+                        <figure className='flex justify-center items-center shadow bg-secondary ticker-pink'><Image width="100" height="100" src="/napa.png"/></figure>
+                        <figure className='flex justify-center items-center shadow bg-secondary ticker-blue'><Image width="100" height="100" src="/guante.png"/></figure>
+                      
+                </div>    
+            </div>
+        </section>
+        </div>
+    </div>
+  )
+}
+
+
+
